ffadf02fb3947d7c5d2bc670663c6ac5c948f76e
aida naderi <aida.naderii@gmail.com>
2012-06-12 00:24:55 +0430

    design slider

---
+++ slider.js
@@ -0,0 +1,35 @@
+$(function(){
+	var last=0;
+	var btns=$('div.slider ul.btns a');
+	var total=btns.length;
+	var active = 0;
+	
+	function gotoSlide(n){
+		
+		if(n<0) n = total-1;
+		if(n>=total) n = 0;
+		active=n;
+		left=-950* n;
+		$('div.train').animate({
+			left:left+'px'
+		},500,'linear');
+		
+		btns.eq(last).removeClass('active');
+		
+		btns.eq(n).addClass('active');
+		last=n;
+	}
+	setInterval(function() {
+      gotoSlide(active+1);
+	}, 3000);
+	btns.click(function(){
+		gotoSlide(btns.index(this));
+	});
+	
+	$('div.slider span.prev').click(function(){
+		gotoSlide(active-1);	
+	});
+	$('div.slider span.next').click(function(){
+		gotoSlide(active+1);	
+	});
+});
